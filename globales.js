2b43910645e377ecc27bc1d003f3cd1210fe178f
Sergio Gómez <sgomez@vise.com.mx>
2021-05-03 00:40:24 -0500

    se agrega url

--- globales.js
+++ globales.js
@@ -1,4 +1,4 @@
 const APP_ID ='JzxZCzLOuQr5mD2P5EEV';
 const APP_CODE ='la43il1x8-rytstctYkpoQ';
-const HOST = 'http://localhost:8080';
-
+//const HOST = 'http://localhost:8080';
+const HOST = 'https://sergiogomez-here.herokuapp.com';
